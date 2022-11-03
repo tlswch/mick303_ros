8561a75c5770d677b069acf7ed04dc61ffdc6dfc
top752 <hluo@hzaowa.com>
2022-11-03 10:25:35 +0000

    更新 'drpy.ym.js'

--- drpy.ym.js
+++ drpy.ym.js
@@ -1,4 +1,4 @@
-import './util.ym.js'
-import dr from './drpy.min.js'
+import 'https://agit.ai/top752/ros/raw/branch/master/libs/util.ym.js'
+import dr from 'https://agit.ai/top752/ros/raw/branch/master/drpy.ym.js'
  
 __JS_SPIDER__ = dr.DRPY()
